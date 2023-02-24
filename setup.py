dc3ce62fba0f34ef1c803ec2e143bfdf4e5fc39c
PyndyalaCoder <114963399+PyndyalaCoder@users.noreply.github.com>
2023-02-23 16:39:56 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pynocturnal',
+    name='lunarinfo',
     version='0.0.1',
     author='Siddhu Pendyala',
     author_email='elcientifico.pendyala@hotmail.com',
