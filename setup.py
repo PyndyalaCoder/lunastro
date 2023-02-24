c2cdcf516844c38ebcd28df9350aa960eee989d9
PyndyalaCoder <114963399+PyndyalaCoder@users.noreply.github.com>
2023-02-24 11:30:09 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='lunation',
-    version='0.0.3',
+    name='lunastro',
+    version='0.0.1',
     author='Siddhu Pendyala',
     author_email='elcientifico.pendyala@hotmail.com',
-    description='python package for lunar information',
+    description='python package for lunar and solar information',
     long_description = long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/PyndyalaCoder/lunation',
+    url='https://github.com/PyndyalaCoder/lunastro',
     project_urls = {
-        "Bug Tracker": "https://github.com/PyndyalaCoder/lunation/issues"
+        "Bug Tracker": "https://github.com/PyndyalaCoder/lunastro/issues"
     },
     license='MIT',
-    packages=['lunation'],
-    install_requires=['requests', 'math'],
+    packages=['lunastro'],
+    install_requires=['requests'],
 )
