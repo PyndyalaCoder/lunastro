d6c2d2f98b84cd426af825ec5f1f8c1bf4530be8
PyndyalaCoder <114963399+PyndyalaCoder@users.noreply.github.com>
2023-02-24 17:22:54 -0800

    Update sun.py

--- lunastro/sun.py
+++ lunastro/sun.py
@@ -33,5 +33,5 @@ class Sun:
                  temptwo = math.cos(2*g)
                  # solar distance is in astronomical units
                  solardistance = 1.00014 - 0.01671*tmp - 0.00014*temptwo
-                 return solardistance * 92,955,807.3 # miles
+                 return solardistance * 92955807.3 # miles
   
