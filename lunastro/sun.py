46cdc7c4013e4efd4b99fdd5b84abb7b8f4dfab3
PyndyalaCoder <114963399+PyndyalaCoder@users.noreply.github.com>
2023-03-05 08:44:58 -0800

    Update sun.py

--- lunastro/sun.py
+++ lunastro/sun.py
@@ -179,7 +179,7 @@ class Sun:
         # Apparent magnitude of Sun at a distance of 1 AU
         sun_magnitude_1au = -26.74
         # Calculate the apparent magnitude of the Sun at the current distance
-        sun_magnitude = sun_magnitude_1au - 2.5 * math.log10(self.solardistance()/92955807.273026)
+        sun_magnitude = sun_magnitude_1au - 2.5 * math.log10(self.solardistance()/92955807.3) # to account for miles, we divide
         return sun_magnitude
     
 
