570c927a0b7529a2baae154d3a9ab3e27a10ff41
PyndyalaCoder <114963399+PyndyalaCoder@users.noreply.github.com>
2023-03-06 17:49:06 -0800

    Update constellation.py

--- lunastro/constellation.py
+++ lunastro/constellation.py
@@ -1,108 +1,81 @@
+import math
 class Stellar:
-            def getViewableConstellations(self, latitude):
-                        constellations = {
-                              "aquarius": 0,
-                              "aries": 2,
-                              "cancer": 2,
-                              "capricornus": 1,
-                              "gemini": 2,
-                              "leo": 0,
-                              "libra": 1,
-                              "ophiuchus":0,
-                              'pisces': 0,
-                              'sagittarius':1,
-                              'scorpius':1,
-                              'taurus': 0,
-                              'virgo': 0,
-                              'andromeda': 2,
-                              'aquila': 0,
-                              'ara': 1,
-                              "argo navis: Carina Puppis Vela": 1,
-                              'auriga': 2,
-                              'Boötes': 2,
-                              'Canis Major': 1,
-                              'Canis Minor': 0,
-                              'cassiopeia': 2,
-                              'centaurus': 1,
-                              'cepheus': 1,
-                              'cetus': 0,
-                              'coma berenices': 1,
-                              'corona australis': 1,
-                              'corona borealis': 2,
-                              'corvus and crater': 1,
-                              'cygnus': 2,
-                              'delphinus': 2,
-                              'draco': 2,
-                              'equuleus': 2,
-                              'eridanus': 0,
-                              'hercules': 2,
-                              'hydra': 0,
-                              'lepus': 1,
-                              'lupus': 1,
-                              'lyra': 2,
-                              'orion': 0,
-                              'pegasus': 2,
-                              'perseus': 2,
-                              'piscis austrinus': 1,
-                              'sagitta': 2,
-                              'serpens': 0,
-                              'triangulum': 2,
-                              'ursa major': 2,
-                              'ursa minor': 2,
-                              # animal constellations
-                              'apus': 1,
-                              'camelopardalis': 2,
-                              'canes venatici': 2,
-                              'chameleon': 1,
-                              'comlumba': 1,
-                              'dorado': 1,
-                              'grus': 1,
-                              'hydrus':1,
-                              'indus':1,
-                              'lacerta': 2,
-                              'leo minor': 2,
-                              'lynx': 2,
-                              'monoceros': 0,
-                              'musca': 1,
-                              'pavo': 1,
-                              'phoenix': 1,
-                              'tucana': 1,
-                              'volans': 1,
-                              'vulpecula': 2, 
-                              # modern constellations
-                              'antlia': 1,
-                              'caelum': 1,
-                              'circinus': 1,
-                              'crux': 1,
-                              'fornax': 1,
-                              'horologium': 1,
-                              'mensa': 1,
-                              'microscopium':1,
-                              'norma': 1,
-                              'octans': 1,
-                              'pictor': 1,
-                              'pyxis': 1,
-                              'reticulum': 1,
-                              'sculptor': 1,
-                              'scutum': 1,
-                              'sextans': 0,
-                              'telescsopium': 1,
-                              'triangulum australe': 1,
+           # as of march 6th, I updated the function to make it more accurate, it takes into account height and lat_min/lat_max as well.
+           def get_viewable_constellations(self, latitude, height):
+                """
+                Get a list of viewable constellations based on latitude.
+
+                Args:
+                latitude (float): The latitude in degrees.
+
+                Returns:
+                list: A list of viewable constellations.
+                """
+                constellation_data = {
+                'Andromeda': {'lat_min': 0, 'lat_max': 90},
+                'Aquarius': {'lat_min': -65, 'lat_max': 10},
+                'Aquila': {'lat_min': -90, 'lat_max': 50},
+                'Aries': {'lat_min': -90, 'lat_max': 60},
+                'Auriga': {'lat_min': 0, 'lat_max': 90},
+                'Boötes': {'lat_min': 0, 'lat_max': 90},
+                'Cancer': {'lat_min': -90, 'lat_max': 60},
+                'Canes Venatici': {'lat_min': 0, 'lat_max': 90},
+                'Canis Major': {'lat_min': -90, 'lat_max': 60},
+                'Capricornus': {'lat_min': -90, 'lat_max': 10},
+                'Cassiopeia': {'lat_min': 0, 'lat_max': 90},
+                'Centaurus': {'lat_min': -60, 'lat_max': 30},
+                'Cepheus': {'lat_min': 0, 'lat_max': 90},
+                'Cetus': {'lat_min': -90, 'lat_max': 20},
+                'Corona Borealis': {'lat_min': 0, 'lat_max': 90},
+                'Corvus': {'lat_min': -60, 'lat_max': 20},
+                'Crater': {'lat_min': -90, 'lat_max': 5},
+                'Cygnus': {'lat_min': 30, 'lat_max': 90},
+                'Delphinus': {'lat_min': 0, 'lat_max': 90},
+                'Draco': {'lat_min': 0, 'lat_max': 90},
+                'Equuleus': {'lat_min': 0, 'lat_max': 90},
+                'Gemini': {'lat_min': -60, 'lat_max': 90},
+                'Hercules': {'lat_min': 0, 'lat_max': 90},
+                'Hydra': {'lat_min': -60, 'lat_max': 20},
+                'Leo': {'lat_min': -90, 'lat_max': 60},
+                'Leo Minor': {'lat_min': 0, 'lat_max': 90},
+                'Libra': {'lat_min': -60, 'lat_max': 20},
+                'Lupus': {'lat_min': -60, 'lat_max': 20},
+                'Lynx': {'lat_min': 30, 'lat_max': 90},
+                'Lyra': {'lat_min': 30, 'lat_max': 90},
+                'Monoceros': {'lat_min': -60, 'lat_max': 20},
+                'Ophiuchus': {'lat_min': -80, 'lat_max': 10},
+                'Orion': {'lat_min': -85, 'lat_max': 85},
+                'Pegasus': {'lat_min': 0, 'lat_max': 90},
+                'Perseus': {'lat_min': 0, 'lat_max': 90},
+                'Pisces': {'lat_min': -90, 'lat_max': 20},
+                'Piscis Austrinus': {'lat_min': -90, 'lat_max': -30},
+                'Sagitta': {'lat_min': 0, 'lat_max': 90},
+                'Sagittarius': {'lat_min': -55, 'lat_max': 20},
+                'Scorpius': {'lat_min': -40, 'lat_max': -55},
+                'Sculptor': {'lat_min': -90, 'lat_max': 20},
+                'Scutum': {'lat_min': -10, 'lat_max': 90},
+                'Serpens': {'lat_min': -80, 'lat_max': 10},
+                'Taurus': {'lat_min': -65, 'lat_max': 90},
+                'Triangulum': {'lat_min': 0, 'lat_max': 90},
+                'Triangulum Australe': {'lat_min': -90, 'lat_max': -25},
+                'Ursa Major': {'lat_min': 0, 'lat_max': 90},
+                'Ursa Minor': {'lat_min': 0, 'lat_max': 90},
+                'Vela': {'lat_min': -60, 'lat_max': -45},
+                'Virgo': {'lat_min': -60, 'lat_max': 20},
+                'Volans': {'lat_min': -75, 'lat_max': -45},
+                'Vulpecula': {'lat_min': 0, 'lat_max': 90}
                         }
-                        hemisphere = 0
-                        if latitude > 0:
-                            hemisphere = 2
-                        else:
-                            hemisphere = 1
 
-                        viewable = []
-                        for k,v in constellations.items():
-                            if v == hemisphere:
-                                viewable.append(k)
-                            if v == 0:
-                                viewable.append(k)
-
-
-                        return viewable
+                 constellations = []
+                 count = 0
+                 for name, data in constellation_data.items():
+                     # Adjust latitude range based on observer height
+                     adj_degrees = 180 / math.pi * math.asin(6371 / (6371 + height))
+                     lat_min = max(data['lat_min'] - adj_degrees, -90)
+                     lat_max = min(data['lat_max'] + adj_degrees, 90)
+                     if latitude >= lat_min and latitude <= lat_max:
+                         constellations.append(name)
+                         count += 1
+                 return constellations
 
 
